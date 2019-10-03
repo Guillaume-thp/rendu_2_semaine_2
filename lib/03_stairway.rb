507a150772a5d1f98073199ca3083dd6f12a1ab7
Guillaume Vidale <guillaume.vidale@gmail.com>
2019-10-03 19:18:17 +0200

    ajout du fichier 3

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,5 +1,42 @@
-def cinq_six
+def init #permet de ne jamais retomber à 0 car à usage unique
+    @marche_initiale = 0
+end
 
-def un
+def lancer_de_des #lancé de dé aléatoire / affiche le résultat
+    @lancer = rand (1..6)
+    puts @lancer
+    
+ end
 
-def deux_trois_quatre
+def resultat_des #définit ce qu'il se passe si 5 ou 6 aux dés
+
+    if @lancer == 5 || @lancer == 6
+    puts "Tu avances d'une marche. Tu es à présent sur la marche #{@marche_initiale}"
+    @marche_initiale +=1
+
+    elsif @lancer == 1 && @marche_initiale > 0
+    puts "Tu descends d'une marche. Tu es à présent sur la marche #{@marche_initiale}"
+    @marche_initiale -=1
+
+    else @lancer == 2 || @lancer == 3 || @lancer == 4
+    puts "Tu restes où tu es. Tu es à présent sur la marche #{@marche_initiale}"
+    @marche_initiale
+    end
+
+end
+
+def rejouer_des
+    if @marche_initiale < 10
+    perform
+    else puts "Bravo, tu as atteint la 10ème marche!"
+    end
+end
+
+def perform
+    lancer_de_des
+    resultat_des
+    rejouer_des
+end
+
+init
+perform
